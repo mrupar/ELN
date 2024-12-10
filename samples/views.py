1120ddf13fa407c8e4ce2ef4e6d7ce79b867d49f
mrupar <miha.rupar33@gmail.com>
2024-12-10 18:33:25 +0100

    bug fix home page

--- samples/views.py
+++ samples/views.py
@@ -1,7 +1,4 @@
 from django.shortcuts import render
 
-from django.http import HttpResponse
-
-
 def index(request):
-    return HttpResponse("Hello, world. You're at the samples index.")
+    return render(request, 'samples/samples.html')
